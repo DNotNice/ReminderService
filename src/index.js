24001fbde5095abfc1f181e845376710003c3c33
Devansh Pandey <todevanshp@gmail.com>
2023-01-31 01:34:07 +0530

    Added logic to subscribe message queue

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 const express  = require('express');
 const bodyParser = require('body-parser')
-const {PORT} = require('./config/server-config')
-const {sendBasicEmail} = require('./services/email-service')
-const setUpAndStartServer = ()=>{
- const app = express();
- const TicketController = require('./controllers/ticket-controller')
- const jobs = require('./utils/job');
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({extended:true}));
- app.post('/api/v1/tickets' , TicketController.create)
+const {PORT ,REMINDER_BINDING_KEY} = require('./config/server-config')
+const EmailService = require('./services/email-service')
+const {createChannel , subscribeMessage} =require('./utils/messageQueue')
+const TicketController = require('./controllers/ticket-controller')
+const jobs = require('./utils/job');
 
+const setUpAndStartServer = async()=>{
+ const app = express();
+ app.use(bodyParser.json()); 
+ app.use(bodyParser.urlencoded({extended:true}));
+
+ app.post('/api/v1/tickets' , TicketController.create);
+ 
+ const channel = await createChannel();
+ subscribeMessage(channel, EmailService.subscribeEvents , REMINDER_BINDING_KEY);
+ 
  app.listen(PORT, ()=>{
     console.log(`server started at port ${PORT}` );
-    jobs();
-    // sendBasicEmail(
-    //     'support@noti.com',
-    //     'dewanshpandey@gmail.com',
-    //     'Notification about your next flight',
-    //     'HEY , you have your flight in the next 48 hours'
-    // );
- })
+    //jobs();
+   
+ });
 }
 setUpAndStartServer();
